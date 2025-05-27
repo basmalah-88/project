802ad6cd1f022e77616e6eaeee84213527c4a869
basmalah-88 <basmalahyosif@gmail.com>
2025-05-27 19:37:22 +0300

    Update m.py

--- m.py
+++ m.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import io
-from code_editor import code_editor
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
